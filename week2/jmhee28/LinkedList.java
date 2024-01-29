72ee17438e48ff6493418b91b1471984e5138c96
Joung MyeongHee <jmhee3411@gmail.com>
2024-01-29 22:23:28 +0900

    fix: DLL fixed

--- week2/jmhee28/LinkedList.java
+++ week2/jmhee28/LinkedList.java
@@ -1,15 +1,17 @@
+import java.util.Arrays;
 
 public class LinkedList<E> {
+    // Doubly Linked List
 
-    Node<E> first;
-    Node<E> last;
+    Node<E> head;
+    Node<E> tail;
     private int size = 0;
 
     public LinkedList() {
-        this.first = new Node<>(null, null, null);
-        this.last = new Node<>(null, null, null);
-        this.first.next = this.last;
-        this.last.prev = this.first;
+        this.head = new Node<>(null, null, null);
+        this.tail = new Node<>(null, null, null);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
     }
     private static class Node<E> {
         E item;
@@ -37,11 +39,11 @@ public class LinkedList<E> {
 
     // index 위치의 노드를 반환하는 메소드
     public E get(int index) {
-        Node cur = this.first;
+        Node cur = this.head.next;
         for (int i = 0; i < index; i++) {
             cur = cur.next;
         }
-        return (E) cur;
+        return (E) cur.item;
     }
 
     // 리스트에 특정 요소가 있는지 확인하는 메소드
@@ -52,9 +54,9 @@ public class LinkedList<E> {
     // 리스트를 배열로 반환하는 메소드
     public Object[] toArray() {
         Object[] arr = new Object[this.size];
-        Node cur = this.first;
+        Node cur = this.head.next;
         for (int i = 0; i < this.size; i++) {
-            arr[i] = cur;
+            arr[i] = cur.item;
             cur = cur.next;
         }
         return arr;
@@ -63,31 +65,24 @@ public class LinkedList<E> {
     // ************* Add Func ***************
 
     // 리스트의 맨 앞에 노드를 추가하는 메소드
-    public void addFirst(E data){
-
-        Node newNode = new Node(data, this.first, null);
-        this.first = newNode;
-
-        if (newNode.next == null) {
-            this.last = newNode;
-        }
+    public void addHead(E data){
+        Node newNode = new Node(data, this.head.next, this.head);
+        this.head.next.prev = newNode;
+        this.head.next = newNode;
         this.size++;
     }
 
     // 리스트의 맨 뒤에 노드를 추가하는 메소드
-    public void addLast(E data) {
-        Node newNode = new Node(data, null, this.last);
-        this.last = newNode;
-
-        if (newNode.prev == null) {
-            this.first = newNode;
-        }
+    public void addTail(E data) {
+        Node newNode = new Node(data, this.tail, this.tail.prev);
+        this.tail.prev.next = newNode;
+        this.tail.prev = newNode;
         this.size++;
     }
 
     // 원하는 index 위치에 노드를 추가하는 메소드
     public void add(int index, E data) {
-        Node cur = this.first;
+        Node cur = this.head.next;
         for (int i = 0; i < index; i++) {
             cur = cur.next;
         }
@@ -100,22 +95,27 @@ public class LinkedList<E> {
     // ********* Delete Func **************
 
     // 리스트 맨 앞 노드를 제거하는 메소드
-    public void removeFirst() {
-        this.first = this.first.next;
-        this.first.prev = null;
+    public void removeHead() {
+        if(this.isEmpty()) return;
+
+        this.head.next = this.head.next.next;
+        this.head.next.prev = this.head;
         this.size--;
     }
 
     // 리스트 맨 뒤 노드를 제거하는 메소드
-    public void removeLast() {
-        this.last = this.last.prev;
-        this.last.next = null;
+    public void removeTail() {
+        if(this.isEmpty()) return;
+        this.tail.prev = this.tail.prev.prev;
+        this.tail.prev.next = this.tail;
         this.size--;
     }
 
     // 리스트 index 위치의 노드를 제거하는 메소드
     public void remove(int index) {
-        Node cur = this.first;
+        if(this.isEmpty()) return;
+
+        Node cur = this.head.next;
         for (int i = 0; i < index; i++) {
             cur = cur.next;
         }
@@ -124,4 +124,37 @@ public class LinkedList<E> {
         this.size--;
     }
 
+    public void printDLL() {
+        System.out.printf("size: %d\n", this.size());
+        System.out.println(Arrays.toString(this.toArray()));
+    }
+    // Test DLL
+    public static void main(String[] args) {
+        LinkedList<Integer> dll = new LinkedList<>();
+        dll.addTail(1);
+        dll.addTail(2);
+        dll.addTail(3);
+        dll.addTail(4);
+        dll.addTail(5);
+        dll.addTail(6);
+        dll.addTail(7);
+        dll.printDLL();
+
+        dll.remove(3);
+        dll.printDLL();
+
+        dll.removeTail();
+        dll.printDLL();
+
+        System.out.println(dll.get(3));
+
+        dll.removeHead();
+        dll.printDLL();
+
+        dll.addHead(0);
+        dll.printDLL();
+
+        dll.addTail(0);
+        dll.printDLL();
+    }
 }
